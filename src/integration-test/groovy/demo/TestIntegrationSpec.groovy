855f4d575273ff65dcaba5a3069daab15019bc93
sdelamo <sergio.delamo@softamo.com>
2017-06-29 10:42:53 +0200

    first commit

---
+++ src/integration-test/groovy/demo/TestIntegrationSpec.groovy
@@ -0,0 +1,12 @@
+package demo
+
+import spock.lang.Specification
+import grails.testing.mixin.integration.Integration
+
+@Integration
+class TestIntegrationSpec extends Specification {
+    def "test"() {
+        expect: 
+        false
+    }
+} 
